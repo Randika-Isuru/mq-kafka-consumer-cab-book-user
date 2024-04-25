be406e4096e9dacacca52005368686068ca8519e
Randika Isuru Vijayanga <randikaisuru@gmail.com>
2024-04-26 01:12:38 +0800

    Initial commit kafka consumer

---
+++ src/main/java/com/example/mqkafkaconsumercabbookuser/MqKafkaConsumerCabBookUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.mqkafkaconsumercabbookuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqKafkaConsumerCabBookUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqKafkaConsumerCabBookUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mqkafkaconsumercabbookuser/MqKafkaConsumerCabBookUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mqkafkaconsumercabbookuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqKafkaConsumerCabBookUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
